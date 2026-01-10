14a1618fa31b8b5bd0ce557cb78b11fe65bce26c
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-01-10 17:35:24 +0900

    implement lexer

--- asmcalc.go
+++ asmcalc.go
@@ -1,5 +1,11 @@
 package asmcalc
 
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
 type TokenType int
 
 const (
@@ -19,4 +25,81 @@ type Token struct {
 	Type  TokenType
 	Value int
 	Name  string
+}
+
+type compiler struct {
+	input    string
+	pos      int
+	tokens   []Token
+	tokenpos int
+}
+
+func lex(input []byte) ([]Token, error) {
+	tokens := make([]Token, 0)
+	pos := 0
+
+	for pos < len(input) {
+		ch := input[pos]
+		if unicode.IsSpace(rune(ch)) {
+			pos++
+			continue
+		}
+		if unicode.IsDigit(rune(ch)) {
+			value := 0
+			for pos < len(input) && unicode.IsDigit(rune(input[pos])) {
+				value = value*10 + int(input[pos]-'0')
+				pos++
+			}
+			tokens = append(tokens, Token{Type: TOK_NUM, Value: value})
+			continue
+		}
+		if unicode.IsLetter(rune(ch)) {
+			var name strings.Builder
+			for pos < len(input) && unicode.IsLetter(rune(input[pos])) {
+				name.WriteByte(input[pos])
+				pos++
+			}
+			nameStr := name.String()
+			if nameStr == "set" {
+				tokens = append(tokens, Token{Type: TOK_ASSIGN})
+			} else {
+				tokens = append(tokens, Token{Type: TOK_IDENT, Name: nameStr})
+			}
+			continue
+		}
+		if ch == '+' {
+			tokens = append(tokens, Token{Type: TOK_PLUS})
+			pos++
+			continue
+		}
+		if ch == '-' {
+			tokens = append(tokens, Token{Type: TOK_MINUS})
+			pos++
+			continue
+		}
+		if ch == '*' {
+			tokens = append(tokens, Token{Type: TOK_MUL})
+			pos++
+			continue
+		}
+		if ch == '/' {
+			tokens = append(tokens, Token{Type: TOK_DIV})
+			pos++
+			continue
+		}
+		if ch == '(' {
+			tokens = append(tokens, Token{Type: TOK_LPAREN})
+			pos++
+			continue
+		}
+		if ch == ')' {
+			tokens = append(tokens, Token{Type: TOK_RPAREN})
+			pos++
+			continue
+		}
+		return nil, fmt.Errorf("unknown char: %c", ch)
+	}
+	tokens = append(tokens, Token{Type: TOK_EOF})
+
+	return tokens, nil
 }
---
+++ asmcalc_test.go
@@ -0,0 +1,40 @@
+package asmcalc
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCompile(t *testing.T) {
+	tokens, err := lex([]byte("3 + 5 * (2 - 8)"))
+	if err != nil {
+		t.Fatalf("lexing failed: %v", err)
+	}
+	for _, tok := range tokens {
+		switch tok.Type {
+		case TOK_NUM:
+			fmt.Print(tok.Value)
+		case TOK_PLUS:
+			fmt.Printf("+")
+		case TOK_MINUS:
+			fmt.Printf("-")
+		case TOK_MUL:
+			fmt.Printf("*")
+		case TOK_DIV:
+			fmt.Printf("/")
+		case TOK_LPAREN:
+			fmt.Printf("(")
+		case TOK_RPAREN:
+			fmt.Printf(")")
+		case TOK_IDENT:
+			fmt.Print(tok.Name)
+		case TOK_ASSIGN:
+			fmt.Printf("=")
+		case TOK_EOF:
+			fmt.Println("")
+		default:
+			println(tok.Type)
+			fmt.Printf("UNKNOWN ")
+		}
+	}
+}
