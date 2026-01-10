688efe7414c76cba9a77aae69006cb106ff81483
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-01-10 17:14:50 +0900

    add expression type and struct for tokenizer

--- asmcalc.go
+++ asmcalc.go
@@ -1,1 +1,22 @@
 package asmcalc
+
+type TokenType int
+
+const (
+	TOK_NUM TokenType = iota
+	TOK_PLUS
+	TOK_MINUS
+	TOK_MUL
+	TOK_DIV
+	TOK_LPAREN
+	TOK_RPAREN
+	TOK_IDENT
+	TOK_ASSIGN
+	TOK_EOF
+)
+
+type Token struct {
+	Type  TokenType
+	Value int
+	Name  string
+}
