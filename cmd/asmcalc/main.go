270f54bcc51eb1f168dff252d545a1ea9717fdd5
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2026-04-20 20:22:10 +0900

    add -f flag to read expression from file

--- cmd/asmcalc/main.go
+++ cmd/asmcalc/main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
-	"github.com/mattn/asmcalc"
+	"flag"
+	"fmt"
 	"os"
+
+	"github.com/mattn/asmcalc"
 )
 
 func main() {
-	compiler := asmcalc.NewCompiler(os.Args[1])
+	filename := flag.String("f", "", "read expression from file")
+	flag.Parse()
+
+	var expr string
+	if *filename != "" {
+		b, err := os.ReadFile(*filename)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		expr = string(b)
+	} else {
+		if flag.NArg() < 1 {
+			fmt.Fprintln(os.Stderr, "usage: asmcalc [-f file] expr")
+			os.Exit(1)
+		}
+		expr = flag.Arg(0)
+	}
+
+	compiler := asmcalc.NewCompiler(expr)
 	compiler.Lex()
 	compiler.Compile(os.Stdout)
 }
